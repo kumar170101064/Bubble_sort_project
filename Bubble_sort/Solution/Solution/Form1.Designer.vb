792f5e71eadc987bd3ded1e2b80341b2460a3c95
kumar170101064 <kumar170101064@iitg.ac.in>
2019-07-25 01:00:31 +0530

    final submission

---
+++ Bubble_sort/Solution/Solution/Form1.Designer.vb
@@ -0,0 +1,245 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Dim ChartArea2 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend2 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series2 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Me.sort = New System.Windows.Forms.Button()
+        Me.input = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lb = New System.Windows.Forms.Label()
+        Me.start = New System.Windows.Forms.Button()
+        Me.bar = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.clock = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.label = New System.Windows.Forms.Label()
+        Me.keys = New System.Windows.Forms.TextBox()
+        Me.list = New System.Windows.Forms.RichTextBox()
+        Me.lebel = New System.Windows.Forms.RichTextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ex = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.r1 = New System.Windows.Forms.RadioButton()
+        Me.r2 = New System.Windows.Forms.RadioButton()
+        Me.Label3 = New System.Windows.Forms.Label()
+        CType(Me.bar, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'sort
+        '
+        Me.sort.BackColor = System.Drawing.Color.DarkSlateGray
+        resources.ApplyResources(Me.sort, "sort")
+        Me.sort.Name = "sort"
+        Me.sort.UseVisualStyleBackColor = False
+        '
+        'input
+        '
+        Me.input.BackColor = System.Drawing.SystemColors.ControlDark
+        resources.ApplyResources(Me.input, "input")
+        Me.input.Name = "input"
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Name = "Label1"
+        '
+        'Label2
+        '
+        resources.ApplyResources(Me.Label2, "Label2")
+        Me.Label2.Name = "Label2"
+        '
+        'lb
+        '
+        resources.ApplyResources(Me.lb, "lb")
+        Me.lb.BackColor = System.Drawing.Color.Tomato
+        Me.lb.Name = "lb"
+        '
+        'start
+        '
+        Me.start.BackColor = System.Drawing.Color.DarkSlateGray
+        Me.start.CausesValidation = False
+        Me.start.Cursor = System.Windows.Forms.Cursors.AppStarting
+        resources.ApplyResources(Me.start, "start")
+        Me.start.ForeColor = System.Drawing.Color.Black
+        Me.start.Name = "start"
+        Me.start.UseCompatibleTextRendering = True
+        Me.start.UseVisualStyleBackColor = False
+        '
+        'bar
+        '
+        Me.bar.BackColor = System.Drawing.Color.Tomato
+        resources.ApplyResources(Me.bar, "bar")
+        Me.bar.BackImageWrapMode = System.Windows.Forms.DataVisualization.Charting.ChartImageWrapMode.Scaled
+        Me.bar.BorderlineColor = System.Drawing.Color.Salmon
+        Me.bar.BorderlineWidth = 5
+        ChartArea2.AlignmentOrientation = System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Horizontal
+        ChartArea2.BackColor = System.Drawing.Color.Salmon
+        ChartArea2.Name = "ChartArea1"
+        ChartArea2.ShadowColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.bar.ChartAreas.Add(ChartArea2)
+        Me.bar.Cursor = System.Windows.Forms.Cursors.Arrow
+        Legend2.Name = "Legend1"
+        Me.bar.Legends.Add(Legend2)
+        Me.bar.Name = "bar"
+        Me.bar.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None
+        Series2.ChartArea = "ChartArea1"
+        Series2.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Series2.IsValueShownAsLabel = True
+        Series2.IsXValueIndexed = True
+        Series2.Legend = "Legend1"
+        Series2.Name = "Value Of Elemnts"
+        Me.bar.Series.Add(Series2)
+        '
+        'clock
+        '
+        Me.clock.Enabled = True
+        Me.clock.Interval = 10000
+        '
+        'label
+        '
+        resources.ApplyResources(Me.label, "label")
+        Me.label.Name = "label"
+        '
+        'keys
+        '
+        Me.keys.BackColor = System.Drawing.SystemColors.ScrollBar
+        resources.ApplyResources(Me.keys, "keys")
+        Me.keys.Name = "keys"
+        '
+        'list
+        '
+        Me.list.AccessibleRole = System.Windows.Forms.AccessibleRole.None
+        resources.ApplyResources(Me.list, "list")
+        Me.list.BackColor = System.Drawing.Color.Tomato
+        Me.list.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.list.Name = "list"
+        Me.list.ReadOnly = True
+        '
+        'lebel
+        '
+        Me.lebel.BackColor = System.Drawing.Color.Tomato
+        Me.lebel.BorderStyle = System.Windows.Forms.BorderStyle.None
+        resources.ApplyResources(Me.lebel, "lebel")
+        Me.lebel.Name = "lebel"
+        Me.lebel.ReadOnly = True
+        '
+        'Label4
+        '
+        resources.ApplyResources(Me.Label4, "Label4")
+        Me.Label4.Name = "Label4"
+        '
+        'ex
+        '
+        Me.ex.BackColor = System.Drawing.Color.DarkSlateGray
+        Me.ex.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        resources.ApplyResources(Me.ex, "ex")
+        Me.ex.Name = "ex"
+        Me.ex.UseVisualStyleBackColor = False
+        '
+        'Label5
+        '
+        resources.ApplyResources(Me.Label5, "Label5")
+        Me.Label5.Name = "Label5"
+        '
+        'Label6
+        '
+        resources.ApplyResources(Me.Label6, "Label6")
+        Me.Label6.Name = "Label6"
+        '
+        'r1
+        '
+        resources.ApplyResources(Me.r1, "r1")
+        Me.r1.Checked = True
+        Me.r1.Name = "r1"
+        Me.r1.TabStop = True
+        Me.r1.UseVisualStyleBackColor = True
+        '
+        'r2
+        '
+        resources.ApplyResources(Me.r2, "r2")
+        Me.r2.Name = "r2"
+        Me.r2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        resources.ApplyResources(Me.Label3, "Label3")
+        Me.Label3.Name = "Label3"
+        '
+        'Form1
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.BackColor = System.Drawing.Color.Tomato
+        Me.CancelButton = Me.ex
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.r2)
+        Me.Controls.Add(Me.r1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.ex)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.lebel)
+        Me.Controls.Add(Me.list)
+        Me.Controls.Add(Me.keys)
+        Me.Controls.Add(Me.label)
+        Me.Controls.Add(Me.bar)
+        Me.Controls.Add(Me.start)
+        Me.Controls.Add(Me.lb)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.input)
+        Me.Controls.Add(Me.sort)
+        Me.HelpButton = True
+        Me.Name = "Form1"
+        CType(Me.bar, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents sort As System.Windows.Forms.Button
+    Friend WithEvents input As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lb As System.Windows.Forms.Label
+    Friend WithEvents start As System.Windows.Forms.Button
+    Friend WithEvents bar As System.Windows.Forms.DataVisualization.Charting.Chart
+    Friend WithEvents clock As System.Windows.Forms.Timer
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents label As System.Windows.Forms.Label
+    Friend WithEvents keys As System.Windows.Forms.TextBox
+    Friend WithEvents list As System.Windows.Forms.RichTextBox
+    Friend WithEvents lebel As System.Windows.Forms.RichTextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents ex As System.Windows.Forms.Button
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents r1 As System.Windows.Forms.RadioButton
+    Friend WithEvents r2 As System.Windows.Forms.RadioButton
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ Bubble_sort/Solution/Solution/Form1.vb
@@ -0,0 +1,278 @@
+﻿Public Class Form1
+
+    Private Sub sort_Click(sender As Object, e As EventArgs) Handles sort.Click
+
+        bar.Visible = False                                                       ' Make chart Visible for showing Animation
+        lebel.Visible = False
+
+        bar.Series("Value Of Elemnts").Points.Clear()   ' Clear the Bar before Going Further
+       
+
+
+        Dim i, dot_count As Integer                     ' For looping
+        Dim j As Integer = 0                            ' For Couting Purpose
+        Dim temp As String = ""                         ' temp For Storing Tempory Strings Which Will be Converted to Doubles
+        Dim temp_string As String = input.Text          ' Storing the Input String
+        Dim input_array() As Double = {}                ' Initializing input_array of Doubles
+
+
+        ' Checking the Input Of Number Of keys If it is Correct Or Not
+
+        For i = 0 To Len(keys.Text) - 1
+            If (keys.Text = "") Or Not (Asc(keys.Text(i)) >= Asc("0") And Asc(keys.Text(i)) <= Asc("9")) Then
+                MessageBox.Show("Have You Given Proper Input Of Number Of Keys in Appropriate Box ???", "Error", MessageBoxButtons.OK, MessageBoxIcon.Question)
+                Exit Sub
+            End If
+        Next
+        ReDim input_array(keys.Text - 1)                        ' Redeclaring Dynamic Array of Size Keys.Text
+
+
+
+
+
+
+        ' Checking the validity of Input String And traversing Strings And convert them to Doubles
+        dot_count = 0
+        For i = 0 To Len(input.Text) - 1
+
+            ' Checking Validity Of String For Differnt Cases And Different Corner Cases
+
+            If (((Asc(temp_string(i)) <= Asc("9")) And (Asc(temp_string(i)) >= Asc("0"))) Or ((Asc(temp_string(i)) = Asc("-")) And (temp = "")) Or (Asc(temp_string(i)) = Asc(".") And dot_count = 0)) Then
+                temp = temp & temp_string(i)
+                If (temp_string(i) = ".") Then
+                    dot_count = dot_count + 1
+                End If
+            ElseIf (Not (temp = "")) And (Asc(temp_string(i)) = Asc(" ")) And Not (temp_string(i) = ".") Then
+                input_array(j) = CDbl(temp)
+                temp = ""
+                j = j + 1  'j is array index for the input array we are generating
+                dot_count = 0
+            ElseIf (temp = "") And temp_string(i) = " " Then
+                temp = ""
+
+
+            Else    ' Else For Other String Which Are not Valid
+                If MessageBox.Show("Syntax Error: This Type of Input is not Valid....." + temp + temp_string(i) + "...is Not Valid ", "Syntax error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error) = Windows.Forms.DialogResult.Cancel Then
+
+                    Me.Close()
+
+                    Exit Sub  ' Exit Sub If Input Is Not Valid And User Wants to Cancel
+
+
+                Else
+                    start.PerformClick()
+
+                    Exit Sub
+                End If
+            End If
+
+            If i = Len(input.Text) - 1 And ((Asc(temp_string(i)) <= Asc("9")) And (Asc(temp_string(i)) >= Asc("0"))) Then
+                input_array(j) = CDbl(temp)
+                j = j + 1
+
+            End If
+
+
+            ' Checking If Number Of Inputs Are Properly IN Input Or Not
+            If (j = keys.Text) Then   ' Breaking Loop If i get All Inputs In input
+                Exit For
+            End If
+
+
+        Next
+
+        
+
+        If (j = keys.Text) Then
+            sort.Enabled = False    ' Disabled Sort Button Untill Sort is Complete
+
+            ' Creating Animations For User|| Creating Chart Of Inputs
+            For i = LBound(input_array) To UBound(input_array)
+
+                Me.bar.Series("Value Of Elemnts").Points.AddXY(i, input_array(i))
+                bar.Series("Value Of Elemnts").Points(i).Color = Color.SteelBlue
+            Next
+
+
+
+            bubble_sort(input_array)    ' Function bubble_sort For Sorting input_array
+
+
+
+            'Some Anim,ations Effects
+            list.Visible = True
+            lb.Visible = True
+
+            'Finally Printing Sorted Atrray in Rich text Box
+            Dim LineOfText As String = String.Join(" ", input_array)
+            list.Text = (LineOfText)
+
+        Else   ' Executes If Less Numbe rOf inputs Are in input
+            If MessageBox.Show("Your Input Is Not Complete. Please Complete It. Choose What To do Next", "Syntax error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Warning) = Windows.Forms.DialogResult.Cancel Then
+
+                Me.Close()
+                Exit Sub
+
+            Else
+
+                sort.Enabled = True
+                Exit Sub
+
+            End If
+
+            End If
+        sort.Enabled = True
+        lebel.Visible = False
+
+    End Sub
+
+
+
+
+
+    Private Sub bubble_sort(ByVal input() As Double)
+        Dim i, j, check_button As Integer                                                      ' Variables for loop management
+
+        bar.Visible = True
+        lebel.Visible = True
+
+        If r1.Checked = True Then                                                ' Check What Type Of sort to be Performed
+            check_button = 0
+        ElseIf r2.Checked = True Then
+            check_button = 1
+        End If
+
+
+        For i = 0 To UBound(input)                                               ' Algorithm Of Bubble Sort // Traverse Elements From 0 to End
+
+            For j = 0 To UBound(input) - i - 1                                   ' Traversing From 0 to unsorted Data
+
+
+                Application.DoEvents()                                           ' Delay Time
+                System.Threading.Thread.Sleep(2000)
+
+
+
+                lebel.Text = ("Checking Both The Numbers If Preecing One is Greater Than Later One")
+                bar.Series("Value Of Elemnts").Points(j).Color = Color.Yellow  ' Change the colors of bars which are checking
+                bar.Series("Value Of Elemnts").Points(j + 1).Color = Color.Yellow
+
+
+                Application.DoEvents()                                          'Delay of Two Seconds
+                System.Threading.Thread.Sleep(2000)
+
+                If (input(j + check_button) > input(j + 1 - check_button)) Then   ' Checking 
+
+                    lebel.Text = ("Swapping takes place since " + CStr(input(j + 1 - check_button)) + " is Less Than " + CStr(input(j + check_button)))
+                    bar.Series("Value Of Elemnts").Points(j).Color = Color.Red   ' Changing the colours of Bars
+                    bar.Series("Value Of Elemnts").Points(j + 1).Color = Color.Red
+
+                    swap(input, j)                                                ' Swap if j+1 th element is less than jth
+
+                    bar.Series("Value Of Elemnts").Points(j).Color = Color.SteelBlue    ' Change the colors of bars which are checking
+                    bar.Series("Value Of Elemnts").Points(j + 1).Color = Color.SteelBlue
+                Else
+                    bar.Series("Value Of Elemnts").Points(j).Color = Color.Green        ' Change the colors of bars which are checking
+                    bar.Series("Value Of Elemnts").Points(j + 1).Color = Color.Green
+                    lebel.Text = (" No Swapping takes place since " + CStr(input(j + check_button)) + " is Less Than " + CStr(input(j + 1 - check_button)))
+                    Application.DoEvents()
+                    System.Threading.Thread.Sleep(1000)
+                    bar.Series("Value Of Elemnts").Points(j).Color = Color.SteelBlue    ' Change the colors of bars which are checking
+                    bar.Series("Value Of Elemnts").Points(j + 1).Color = Color.SteelBlue
+
+                End If
+              
+                lebel.Text = ("")
+
+            Next
+
+            bar.Series("Value Of Elemnts").Points(j).Color = Color.DarkRed
+            Application.DoEvents()                                                  'Delay Of some Time
+            System.Threading.Thread.Sleep(2000)            ' Finally Stabilizing the colors of bars Which Are Sorted
+        Next
+
+        Application.DoEvents()                                                          ' Delay Of some Time
+        System.Threading.Thread.Sleep(3000)
+        lebel.Visible = False
+    End Sub
+
+
+    ' Swap Function For Swapping Elemets
+    Private Sub swap(ByVal input() As Double, ByVal j As Integer)    ' Array(input) and the index(j th and j+1 th Number Are to be swapped) As Arguments
+        Dim temp As Double                                           ' temporary variable for swapping
+
+
+        ' Swap the Numbers
+        temp = input(j)
+        input(j) = input(j + 1)
+        input(j + 1) = temp
+
+        ' Stop there For Some Time
+        Application.DoEvents()
+        System.Threading.Thread.Sleep(3000)
+
+
+        ' Changing the Bars And Colors of Bars 
+        bar.Series("Value Of Elemnts").Points.ElementAt(j + 1).SetValueY(input(j + 1))
+        bar.Series("Value Of Elemnts").Points.ElementAt(j).SetValueY(input(j))
+        
+        Application.DoEvents()
+        System.Threading.Thread.Sleep(2000)
+
+
+    End Sub
+
+    
+
+    Private Sub start_Click(sender As Object, e As EventArgs) Handles start.Click
+        list.Clear()
+        input.Text = ""
+        bar.Series("Value Of Elemnts").Points.Clear()
+
+        keys.Clear()
+        bar.Visible = False
+        lebel.Visible = False
+        lb.Visible = False
+        list.Visible = False
+    End Sub
+
+
+    Private Sub ex_Click_1(sender As Object, e As EventArgs) Handles ex.Click
+        Me.Close()
+        Exit Sub
+    End Sub
+
+    
+    Private Sub Label8_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub bar_Click(sender As Object, e As EventArgs) Handles bar.Click
+
+    End Sub
+
+    Private Sub r1_CheckedChanged(sender As Object, e As EventArgs) Handles r1.CheckedChanged
+        If (bar.Visible = True) And r1.Checked = True Then
+            sort.PerformClick()
+        End If
+    End Sub
+
+    Private Sub r2_CheckedChanged(sender As Object, e As EventArgs) Handles r2.CheckedChanged
+        If (bar.Visible = True) And r2.Checked = True Then
+            sort.PerformClick()
+        End If
+    End Sub
+
+   
+End Class
+
+
+
